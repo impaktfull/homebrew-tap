f5db83a83c7ab5f271c72f7545d05478aebd8fa8
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2026-03-31 11:17:47 +0200

    fix: correct sha

--- Casks/xray.rb
+++ Casks/xray.rb
@@ -1,4 +1,4 @@
-cask "xray" do
+cask "xRay" do
   version "0.0.1"
   url "https://github.com/impaktfull/xray/releases/download/v0.0.1/xRay-0.0.1.dmg"
   sha256 "5d0e83c803aa9b26966933a4fd83af18d2ab3497ced486dc2e2dfb0bbc0caa9c"
