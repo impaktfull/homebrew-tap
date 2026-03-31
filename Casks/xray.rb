43bdb88a13c15af2421e68fc1be99024d1ebbccb
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2026-03-31 11:03:04 +0200

    feat: added 0.0.1 sha & url for xray

--- Casks/xray.rb
+++ Casks/xray.rb
@@ -1,8 +1,8 @@
 cask "xray" do
   version "0.0.1"
-  sha256 "paste_your_sha256_here"
+  sha256 "701ba36f991959f2135504b4b1eb34c53eed98144b4efa40199a1525138b73ca"
 
-  url "https://github.com/impaktfull/xray/releases/download/v#{version}/XRay-#{version}.dmg"
+  url "https://github.com/impaktfull/xray/releases/download/v0.0.1/xray-0.0.1.dmg"
   name "xRay"
   desc "Desktop app for inspecting network calls, logs and key-value stores"
   homepage "https://github.com/impaktfull/xray"
