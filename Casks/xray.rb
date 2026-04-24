643b3fc7d45ea590c3bdc40944dba931ba2b0f3b
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2026-04-24 12:34:30 +0200

    Update sha256 checksum for xray cask

--- Casks/xray.rb
+++ Casks/xray.rb
@@ -1,7 +1,7 @@
 cask "xray" do
   version "0.0.1"
   url "https://github.com/impaktfull/xray/releases/download/v0.0.1/xRay-0.0.1.dmg"
-  sha256 "0d839b8cdf4b7e86f04b2129b11c7c448daafcb4ff15f926eea2ad91bbf38b6b"
+  sha256 "f438cf4efa1515eb465cdc1a667e6ad9266289068f5fa18f825906729e4eda81"
   name "xRay"
   desc "Desktop app for inspecting network calls, logs and key-value stores"
   homepage "https://github.com/impaktfull/xray"
