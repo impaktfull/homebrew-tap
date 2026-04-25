9db850e21b2d19f3866844cf11e17b231187dc7a
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2026-04-25 08:20:48 +0200

    Update xray version to 0.0.2

--- Casks/xray.rb
+++ Casks/xray.rb
@@ -1,6 +1,6 @@
 cask "xray" do
-  version "0.0.1"
-  url "https://github.com/impaktfull/xray/releases/download/v0.0.1/xRay-0.0.1.dmg"
+  version "0.0.2"
+  url "https://github.com/impaktfull/xray/releases/download/v0.0.2/xRay-0.0.2.dmg"
   sha256 "f6291aceee3e1b573e79419c2fc301cff18a99ac8cff9aadbcb1e0063b2a6130"
   name "xRay"
   desc "Desktop app for inspecting network calls, logs and key-value stores"
